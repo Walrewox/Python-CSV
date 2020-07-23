93a6e9e9e4bb3fc8a2e971dc97508a4a3a9388cb
Walrewox <saske.pawlyuk2012@gmail.com>
2020-07-23 15:02:11 +0300

    Attempts to form rows of result and ideas without csv module

--- withLibrary.py
+++ withLibrary.py
@@ -6,25 +6,29 @@ months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul
 def readAndWrite(fileObj):
     """ Open and read information from CSV file,
     parse it and write to new file """
-    reader = csv.reader(fileObj)
-    headColumn = next(reader)
+    writeHead = csv.reader(fileObj)
+    next(writeHead)
+
     result = open("withResult.txt","w")
     result.write("Name/Date,")
     date = 0
-    name = 0
-    for row in reader:
+    for row in writeHead:
+        print("foo")
         if(row[1] != date):
             date = row[1]
             year = re.findall(r"\d{4}",date)
             month = re.findall(r"[A-Za-z]{3}",date)
             day = re.findall(r"\d{2}",date)
-            print(f"{year} {month} {day}")
-
             result.write(f"{year[0]}-{months[month[0]]}-{day[0]},")
-    for row in reader:
+    writeHead = None
+    writeBody = csv.reader(fileObj)
+    next(writeBody)
+    name = 0
+    for row in writeBody:
+        print("bar")
         if(row[0] != name):
             name = row[0]
-            result.write(f"{name},\n")
+            result.write(f"{name}",)
 
 csvName = "acme_worksheet.csv"
 file = open(csvName,"r")
--- withoutLib.py
+++ withoutLib.py
@@ -1,14 +1,23 @@
-def Write(values):
-    result = open("result.csv","w")
-    result.write("Name,Date,Info\nGeorge,July 22 2020,Pudge")
-def Read(file):
-    print("Hello")
+import re
 
-names = []
-dates = []
-hours = []
+months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08',
+'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
-worksheet = open("acme_worksheet.csv","r")
-Read(worksheet)
+def write(names,dates):
+    result = open("withoutResult.csv","w")
+    result.write()
+def readNames(fileName):
+    string = open(fileName,"r").read()
+    finded = re.findall(r"[A-Za-z]{4,10} [A-Za-z]{2,10},",string)
+    result = []
+    for i in finded:
+        if(i not in result):
+            result.append(i)
+    return result
 
-worksheet.close()
+
+worksheet = "acme_worksheet.csv"
+names = readNames(worksheet)
+testFile = open("test.txt","w")
+for i in names:
+    testFile.write(f"{i}\n")
