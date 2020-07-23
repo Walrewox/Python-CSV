a593f61ddc6dd7d6600afe8a67e03fc0e6ae83e4
Walrewox <saske.pawlyuk2012@gmail.com>
2020-07-23 19:51:27 +0300

    Upgrade version with library

--- withLibrary.py
+++ withLibrary.py
@@ -6,8 +6,8 @@ months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul
 def readAndWrite(fileName):
     """ Open and read information from CSV file,
     parse it and write header of columns to new file """
-    fileObj = open(fileName,"r")
-    readHead = csv.reader(fileObj)
+    headObj = open(fileName,"r")
+    readHead = csv.reader(headObj)
     next(readHead)
 
     result = open("resultWith.csv","w")
@@ -20,26 +20,23 @@ def readAndWrite(fileName):
             month = re.findall(r"[A-Za-z]{3}",date)
             day = re.findall(r"\d{2}",date)
             result.write(f"{year[0]}-{months[month[0]]}-{day[0]},")
-    fileObj.close()
-    fileObj = open(fileName,'r')
-    readBody = csv.reader(fileObj)
+    headObj.close()
+
+    nameObj = open(fileName,'r')
+    hourObj = open(fileName,'r')
+    readNames = csv.reader(nameObj)
+    readHours = csv.reader(hourObj)
     name = 0
-    for row in readBody:
-        if(row[0] != name):
+    names = []
+    for row in readNames:
+        if(row[0] != name)and(row[0] not in names):
             name = row[0]
+            names.append(name)
             result.write(f"{name},\n")
 
-# def writeBody(fileObj):
-#     """ Read information from CSV file and
-#     write Names and Work Hours to file created in writeHead() function """
-#     writeBody = csv.reader(fileObj)
-#     next(writeBody)
-#     name = 0
-#     for row in writeBody:
-#         print("bar")
-#         if(row[0] != name):
-#             name = row[0]
-#             result.write(f"{name}",)
+    nameObj.close()
+    hourObj.close()
 
-csvName = "acme_worksheet.csv"
-readAndWrite(csvName)
+if __name__ == "__main__":
+    csvName = "acme_worksheet.csv"
+    readAndWrite(csvName)
