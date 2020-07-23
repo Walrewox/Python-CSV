b7079ffa74a4b5e182732717a184d6d2c5a3caec
Walrewox <saske.pawlyuk2012@gmail.com>
2020-07-23 19:18:51 +0300

    New ideas and upgrade outputs, renaming files

--- withLibrary.py
+++ withLibrary.py
@@ -3,33 +3,43 @@ import re
 
 months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08',
 'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
-def readAndWrite(fileObj):
+def readAndWrite(fileName):
     """ Open and read information from CSV file,
-    parse it and write to new file """
-    writeHead = csv.reader(fileObj)
-    next(writeHead)
+    parse it and write header of columns to new file """
+    fileObj = open(fileName,"r")
+    readHead = csv.reader(fileObj)
+    next(readHead)
 
-    result = open("withResult.txt","w")
+    result = open("resultWith.csv","w")
     result.write("Name/Date,")
     date = 0
-    for row in writeHead:
-        print("foo")
+    for row in readHead:
         if(row[1] != date):
             date = row[1]
             year = re.findall(r"\d{4}",date)
             month = re.findall(r"[A-Za-z]{3}",date)
             day = re.findall(r"\d{2}",date)
             result.write(f"{year[0]}-{months[month[0]]}-{day[0]},")
-    writeHead = None
-    writeBody = csv.reader(fileObj)
-    next(writeBody)
+    fileObj.close()
+    fileObj = open(fileName,'r')
+    readBody = csv.reader(fileObj)
     name = 0
-    for row in writeBody:
-        print("bar")
+    for row in readBody:
         if(row[0] != name):
             name = row[0]
-            result.write(f"{name}",)
+            result.write(f"{name},\n")
+
+# def writeBody(fileObj):
+#     """ Read information from CSV file and
+#     write Names and Work Hours to file created in writeHead() function """
+#     writeBody = csv.reader(fileObj)
+#     next(writeBody)
+#     name = 0
+#     for row in writeBody:
+#         print("bar")
+#         if(row[0] != name):
+#             name = row[0]
+#             result.write(f"{name}",)
 
 csvName = "acme_worksheet.csv"
-file = open(csvName,"r")
-readAndWrite(file)
+readAndWrite(csvName)
--- withoutLib.py
+++ withoutLib.py
@@ -3,9 +3,9 @@ import re
 months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08',
 'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
-def write(names,dates):
-    result = open("withoutResult.csv","w")
-    result.write()
+# def write(names,dates):
+#     result = open("resultWithout.csv","w")
+#     result.write()
 def readNames(fileName):
     string = open(fileName,"r").read()
     finded = re.findall(r"[A-Za-z]{4,10} [A-Za-z]{2,10},",string)
@@ -13,11 +13,18 @@ def readNames(fileName):
     for i in finded:
         if(i not in result):
             result.append(i)
+    result.pop(0)
+    result.sort()
     return result
+# def findHours(fileName,namesStr):
+#     string = open(fileName,"r").read()
+#     for i in namesStr:
+
+
 
 
 worksheet = "acme_worksheet.csv"
 names = readNames(worksheet)
-testFile = open("test.txt","w")
+file = open("resultWithout","w")
 for i in names:
-    testFile.write(f"{i}\n")
+    file.write(f"{i}\n")
