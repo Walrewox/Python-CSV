51695cad787a96b7e627cdc176e6c2a32f1093d4
Walrewox <saske.pawlyuk2012@gmail.com>
2020-07-24 20:15:35 +0300

    Writing output hours, rework import and some details. Problems

--- withLibrary.py
+++ withLibrary.py
@@ -1,5 +1,5 @@
 import csv
-import re
+from re import findall
 
 months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08',
 'Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
@@ -8,23 +8,24 @@ def readAndWrite(fileName):
     parse it and write header of columns to new file """
     headObj = open(fileName,"r")
     readHead = csv.reader(headObj)
-    next(readHead)
+    next(readHead) #skip header-row
 
     result = open("resultWith.csv","w")
     result.write("Name/Date,")
-    date = 0
+    dates = []
     for row in readHead:
-        if(row[1] != date):
-            date = row[1]
-            year = re.findall(r"\d{4}",date)
-            month = re.findall(r"[A-Za-z]{3}",date)
-            day = re.findall(r"\d{2}",date)
+        if(row[1] not in dates):
+            dates.append(row[1])
+            year = findall(r"\d{4}",row[1])
+            month = findall(r"[A-Za-z]{3}",row[1])
+            day = findall(r"\d{2}",row[1])
             result.write(f"{year[0]}-{months[month[0]]}-{day[0]},")
     headObj.close()
     """ End of formating headers """
+
     nameObj = open(fileName,'r')
     readNames = csv.reader(nameObj)
-    next(readNames)
+    next(readNames) #skip header-row
     names = []
     for row in readNames:
         if (row[0] not in names):
@@ -33,13 +34,25 @@ def readAndWrite(fileName):
     names.sort()
     """ End of reading names """
     for i in names:
-        hourObj = open(fileName,'r')
-        readHours = csv.reader(hourObj)
         result.write(f"\n{i},")
-        for row in readHours:
-            if(i == row[0]):
-                result.write(f"{row[2]},")
+        hourObj = open(fileName,'r')
+        readHour = csv.reader(hourObj)
+        holydays = []
+        print(holydays)
+        for j in dates:
+            for row in readHour:
+                if(row[1] not in holydays):
+                    print(row[1])
+                    print(j)
+                    if(j == row[1]):
+                        if(i == row[0]):
+                            result.write(f"{row[2]},")
+                    else:
+                        holydays.append(row[1])
+                        result.write(f"0,")
+                        # break
         hourObj.close()
+    print(holydays)
 
 if __name__ == "__main__":
     csvName = "acme_worksheet.csv"
